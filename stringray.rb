f2bbd4efed8064dc07c7d7c887bfd46b60b4886e
Geoffrey Foster <geoff@g-Off.net>
2018-12-10 21:48:07 -0500

    Bumping stringray version to 0.2.0

--- stringray.rb
+++ stringray.rb
@@ -1,8 +1,8 @@
 class Stringray < Formula
   desc "Manipulate and lint your strings and stringsdict files"
   homepage "https://github.com/g-Off/stringray"
-  url "https://github.com/g-Off/stringray/archive/0.1.1.tar.gz"
-  sha256 "0aecc372d5039a1bcf5c150c1d741f7bd92fcb8182d7fc91e06481a6ef0b8804"
+  url "https://github.com/g-Off/stringray/archive/0.2.0.tar.gz"
+  sha256 "54c0dbd6867161d6d54c4690e8953a1ba79440c6a1b5311b5768e6e9744d80b7"
   head "https://github.com/g-Off/stringray.git"
 
   depends_on :xcode
