7fedcf5d70da7b7b6c2c07888897d93dc48e1be7
Geoffrey Foster <geoff@g-Off.net>
2018-12-14 16:33:21 -0500

    adding bottle for stringray

--- stringray.rb
+++ stringray.rb
@@ -10,4 +10,10 @@ class Stringray < Formula
   def install
     system "make", "install", "PREFIX=#{prefix}"
   end
+  
+  bottle do
+    root_url "https://github.com/g-Off/stringray/releases/download/0.3.0"
+    cellar :any_skip_relocation
+    sha256 "f314d08580a7be9e3d34baf87ee05bf0a2ccfea71c7ac15a6ecf50ce5057f3c1" => :mojave
+  end
 end
